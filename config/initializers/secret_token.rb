bf0629ce20f727a8875424f49328c118b73124c2
Mark D. Blackwell <markdblackwell01@gmail.com>
2012-06-29 11:31:23 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartupAbcdefg::Application.config.secret_token = '6fd2b95f56ec87b5846826ed0a0cae224ab93f93f5007b291674e304761b0575820548fc51bd438427cd0f359b7874049341d937050c4d14b76813c6c0b11971'
