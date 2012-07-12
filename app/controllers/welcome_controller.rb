0a4240421b99e308e7edd73c95f186d524f14533
Mark D. Blackwell <markdblackwell01@gmail.com>
2012-07-12 08:48:03 -0400

    Toward memcache.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,15 +8,15 @@ class WelcomeController < ApplicationController
 
   def set_up_amqp
     u = ENV['CLOUDAMQP_URL']
-    b = Bunny.new(u.blank? ? nil : u)
+#    b = Bunny.new(u.blank? ? nil : u)
+    b = Bunny.new
     b.start # Does not return b.
     @bunny_queue = b.queue 'test1'
-    b.exchange('').publish 'Hello from set_up_amqp', :key => 'test1'
+    b.exchange('').publish 'Hello from set_up_amqp in Rails app', :key => 'test1'
   end
 
   def set_up_memcachier
-    Rails.cache.write 'foo', 'Hello from set_up_memcachier'
-
+#    Rails.cache.write 'foo', 'Hello from set_up_memcachier in Rails app'
   end
 
 end
